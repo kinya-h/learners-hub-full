d42a03f921a0990a5f16f9fbe64ba4cb550a1b77
kinya-h <kinyanjuisamwel07@gmail.com>
2025-02-07 09:13:37 +0300

    try fix mysqlclient error

--- learnershubbackend/settings.py
+++ learnershubbackend/settings.py
@@ -110,6 +110,12 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
+del DATABASES['default']['OPTIONS']['sslmode'] 
+DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
